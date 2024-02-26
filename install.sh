a3e99552ec08996e2c5327b0e726a4eb1ebc5a39
devrohitv <142287858+devrohitv@users.noreply.github.com>
2024-02-26 15:24:20 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+pip3 install --user --upgrade buildozer
+
+sudo apt update
+sudo apt install -y git zip unzip openjdk-17-jdk python3-pip autoconf libtool pkg-config zlib1g-dev libncurses5-dev libncursesw5-dev libtinfo5 cmake libffi-dev libssl-dev
